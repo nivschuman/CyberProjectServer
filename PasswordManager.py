15b09ac2fc29df0fcb849c298693c1865f99853a
Niv Schuman <niv.schuman@gmail.com>
2023-11-24 09:40:08 +0200

    create user
    continued working on create user

--- PasswordManager.py
+++ PasswordManager.py
@@ -22,8 +22,27 @@ class PasswordManagerServer:
         body_json = json.loads(body_str)
 
         public_key_bytes = base64.b64decode(body_json["publicKey"])
+        user_name = body_json["userName"]
 
-        res.set_header_value("Content-Length", 0)
+        # todo remove sql injection
+        # check if there already exists a user with given username
+        public_key_str = f"0x{public_key_bytes.hex()}"
+        self.db_cursor.execute(f"SELECT UserName, PublicKey From Users WHERE UserName=\'{user_name}\'")
+        user_with_same_username = self.db_cursor.fetchall() is not None
+        self.db_cursor.execute(f"SELECT UserName, PublicKey From Users WHERE PublicKey=\'{public_key_str}\'")
+        user_with_same_public_key = self.db_cursor.fetchall() is not None
+
+        if user_with_same_username:
+            res.body = "User with this username already exists, choose a different username"
+        elif user_with_same_public_key:
+            res.body = "User with this public key already exists, choose a different public key"
+        else:
+            self.db_cursor.execute(f"INSERT INTO Users (UserName, PublicKey) VALUES (\'{user_name}\', \'{public_key_str}\')")
+            res.body = "Successfully created user"
+
+        res.set_header_value("Content-Length", len(res.body))
+        res.set_header_value("Method", "create_user")
+        res.set_header_value("Content-Type", "ascii string")
 
     def login_request(self, req, res, session):
         pass
